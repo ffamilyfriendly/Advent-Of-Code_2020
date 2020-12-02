84252e03fdfb0366631411332df28126fa273db7
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2020-12-02 20:37:27 +0100

    generated all files

--- genFolders.sh
+++ genFolders.sh
@@ -1,3 +1,9 @@
+#how the generated cpp files should look
+gen_cpp="
+int main(int argc,  char** argv) {
+    
+}"
+
 # generates the folders for each day and each challange
 for i in {1..25}
 do
@@ -5,5 +11,8 @@ do
     mkdir ./day_${i}
     mkdir ./day_${i}/challange_1
     mkdir ./day_${i}/challange_2
+    echo "${gen_cpp}" > ./day_${i}/challange_1/main.cpp
+    echo "${gen_cpp}" > ./day_${i}/challange_2/main.cpp
+    echo -e "all:\n\t\tg++ ./challange_1/main.cpp -o ./challange_1.exe && g++ ./challange_2/main.cpp -o ./challange_2.exe" > ./day_${i}/makefile
 done
 exit 0
