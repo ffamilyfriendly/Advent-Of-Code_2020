711c8dc15855999acbdbed50fe2389edd8f29143
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2020-12-11 15:12:53 +0100

    day 12 challange 1

--- day_10/challange_2/main.cpp
+++ day_10/challange_2/main.cpp
@@ -1,4 +1,42 @@
+// Todays epic music is days n dayz
+// https://open.spotify.com/track/1MGIxsVtqK2npdPmI8Dj0D?si=Cif-G157QRGE40cxWAeUyQ
+
+#include "../../lib/header.h"
+#include <fstream>
+#include <bits/stdc++.h>
+
+vector<int> adapters;
+
+#define debug false
 
 int main(int argc,  char** argv) {
-    
+
+    fstream input;
+
+    #if debug
+        input.open("debug.txt");
+    #else
+        input.open("input.txt");
+    #endif
+
+    if(!input || !input.is_open()) {
+        perror("FILE!?!?!?");
+        exit(EXIT_FAILURE);
+    }
+
+    string line;
+
+    while(getline(input,line)) {
+        adapters.push_back(atoi(line.c_str()));
+    }
+
+    sort(adapters.begin(), adapters.end());
+
+    int differences[3] = {1,1,1};
+
+    for(int i = 0; i < adapters.size(); i++) {
+        differences[(adapters[i+1] - adapters[i])-1]++;
+    }
+
+    cout << "differences[0]:" << differences[0] << " differences[1]:" << differences[1] << " differences[2]:" << differences[2] << " answer:" << differences[0] * differences[2]  << endl;
 }
--- day_11/challange_1/main.cpp
+++ day_11/challange_1/main.cpp
@@ -1,4 +1,68 @@
+#include "../../lib/header.h"
+#include <fstream>
+
+#define debug false
+
+vector<string> seats;
+
+int surrounding(char target, int x, int y, vector<string> rows) {
+    int res = 0;
+    for(int i = y-1; i < y+2; i++) {
+        for(int j = x-1; j < x+2; j++) {
+            res += rows[i][j] == target;
+        }
+    }
+    return res;
+}
 
 int main(int argc,  char** argv) {
     
+    fstream input;
+
+    #if debug
+        input.open("debug.txt");
+    #else
+        input.open("input.txt");
+    #endif
+
+    string line;
+
+    while(getline(input,line)) {
+        seats.push_back('.'+line+'.');
+    }
+
+    seats.push_back(string(seats[0].size(),'.'));
+    seats.insert(seats.begin(),string(seats[0].size(),'.'));
+
+    bool hasChanged = true;
+    vector<string> deltaSeats = seats;
+    int availSeats = 0;
+
+    vector<string> tempSeats = {};
+
+    while(hasChanged) {
+        availSeats = 0;
+        bool thisLoopHasChanged = false;
+        tempSeats = deltaSeats;
+        for(int i = 1; i < deltaSeats.size()-1; i++) {
+            for(int j = 1; j < deltaSeats[i].size()-1; j++) {
+                if(deltaSeats[i][j] == '.') continue;
+
+                if(surrounding('#',j,i,deltaSeats) > 4) {
+                    tempSeats[i][j] = 'L';
+                } else if(surrounding('#',j,i,deltaSeats) == 0) {
+                    tempSeats[i][j] = '#';
+                }
+
+                availSeats += tempSeats[i][j] == '#';
+
+                if(tempSeats[i][j] != deltaSeats[i][j]) thisLoopHasChanged = true;
+            }
+        }
+        deltaSeats = tempSeats;
+        hasChanged = thisLoopHasChanged;
+    } 
+
+    for(string row: deltaSeats) cout << row << endl;
+    cout << availSeats << endl;
 }
